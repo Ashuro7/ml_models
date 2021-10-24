6ba55d4553a34ab8c9486dafd0c9c8ab8de88e78
Ashutosh Roy <ashutoshr@Cenduit.com>
2021-10-24 16:53:38 +0530

    added comments to mnist.py

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -6,12 +6,13 @@ import torch.nn.functional as F
 from torchvision.transforms import transforms
 import torch.optim as optim
 
-
+#Defining Hyperparameters
 num_epochs = 4
 learn_rate = 0.001
 num_input = 28 * 28
 num_class = 10
 
+#Data loading and transformation
 img_transforms = transforms.Compose([transforms.ToTensor()])
 
 train = dataset.MNIST(root='./datasets', train=True, download=True, transform=img_transforms)
@@ -20,7 +21,7 @@ test = dataset.MNIST(root='./datasets', train=False, download=True, transform=im
 train_data = DataLoader(train, batch_size=64, shuffle=True)
 test_data = DataLoader(test, batch_size=64, shuffle=True)
 
-
+#Model architecture definition
 class MNISTNet(nn.Module):
     def __init__(self, inp_s, out_s):
         super().__init__()
@@ -38,11 +39,10 @@ class MNISTNet(nn.Module):
 model = MNISTNet(num_input, num_class)
 
 loss_fn = nn.CrossEntropyLoss()
-opt = optim.Adam(model.parameters(), lr=0.001)
+opt = optim.Adam(model.parameters(), lr=learn_rate)
 
-
-
-for ep in range(1, num_epochs):
+#Training the model
+for ep in range(1, num_epochs+1):
     for idx, data in enumerate(train_data):
         X, y = data
         
@@ -54,12 +54,11 @@ for ep in range(1, num_epochs):
         
         opt.zero_grad()
         
-    print(f'Loss at epoch {ep} was : {loss}')
+    print(f'Loss at epoch {ep} was : {loss:.4f}')
 
-
+#Testing
 total = 0
 correct = 0
-
 
 with torch.no_grad():
     for idx, data in enumerate(test_data):
@@ -72,12 +71,11 @@ with torch.no_grad():
                 correct += 1
                 
 acc = correct/total
-print(f'Accuracy is: {round(acc, 3)}')
+print(f'Accuracy is: {acc:.3f}')
 
-
+#Saving the parameters of the trained model
 model_path = 'mnist.pt'
 
 if acc > 0.90:
     torch.save(model.state_dict(), model_path)
     print('Model saved to ' + model_path)
-
