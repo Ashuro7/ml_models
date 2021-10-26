4318c7aec887e6c52fc9725d4ce165bc2530fe46
ashutosh roy <ashutoshroy34@gmail.com>
2021-10-27 00:22:41 +0530

    added gpu support and refactored the code

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -4,12 +4,16 @@ import torchvision.datasets as dataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 import torch.optim as optim
+from tqdm import tqdm
 
 #Defining Hyperparameters
 num_epochs = 4
 learn_rate = 0.001
 num_input = 28 * 28
 num_class = 10
+batch_s = 64
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 #Data loading and transformation
 img_transforms = transforms.Compose([transforms.ToTensor()])
@@ -17,43 +21,43 @@ img_transforms = transforms.Compose([transforms.ToTensor()])
 train = dataset.MNIST(root='./datasets', train=True, download=True, transform=img_transforms)
 test = dataset.MNIST(root='./datasets', train=False, download=True, transform=img_transforms)
 
-train_data = DataLoader(train, batch_size=64, shuffle=True)
-test_data = DataLoader(test, batch_size=64, shuffle=True)
+train_data = DataLoader(train, batch_size=batch_s, shuffle=True)
+test_data = DataLoader(test, batch_size=batch_s, shuffle=True)
 
 #Model architecture definition
 class MNISTNet(nn.Module):
     def __init__(self, inp_s, out_s):
         super().__init__()
-        self.layer1 = nn.Linear(inp_s, 64)
-        self.layer2 = nn.Linear(64, 64)
-        self.layer3 = nn.Linear(64, 64)
-        self.layer4 = nn.Linear(64, out_s)
+        self.inp = nn.Linear(inp_s, 64)
+        self.hd1 = nn.Linear(64,64)
+        self.out = nn.Linear(64, out_s)
         
-        self.neuralnet = nn.Sequential(self.layer1, nn.ReLU(), self.layer2, nn.ReLU(), self.layer3,
-                                      nn.ReLU(), self.layer4)
+        self.forward_pass = nn.Sequential(self.inp, nn.ReLU(), self.hd1, nn.ReLU(), 
+                                       self.out)
     def forward(self, X):
-        output = self.neuralnet(X)
+        output = self.forward_pass(X)
         return output
     
-model = MNISTNet(num_input, num_class)
+model = MNISTNet(num_input, num_class).to(device)
 
 loss_fn = nn.CrossEntropyLoss()
 opt = optim.Adam(model.parameters(), lr=learn_rate)
 
 #Training the model
 for ep in range(1, num_epochs+1):
-    for idx, data in enumerate(train_data):
-        X, y = data
+    for data in tqdm(train_data):
+        X, y = data        
+        X, y = X.view(-1, 28*28).to(device), y.to(device)
         
-        out = model(X.view(-1, 28*28))
+        out = model(X)
         loss = loss_fn(out, y)
         
         loss.backward()
         opt.step()
         
-        opt.zero_grad()
+        opt.zero_grad(set_to_none=True)
         
-    print(f'Loss at epoch {ep} was : {loss:.4f}')
+    print(f'Epoch: {ep}, Loss: {loss:.4f}')
 
 #Testing
 total = 0
@@ -62,19 +66,19 @@ correct = 0
 with torch.no_grad():
     for idx, data in enumerate(test_data):
         X,y = data
-        output = model(X.view(-1, 28*28))
-        idx_max = torch.argmax(output, dim=1)
-        for idx, out in enumerate(idx_max):
-            total += 1
-            if out == y[idx]:
-                correct += 1
+        X,y = X.view(-1, 28*28).to(device), y.to(device)
+        
+        output = model(X)
+        idx_max = torch.argmax(output, dim=1).to(device)
+        correct += (idx_max == y).sum().item()
+        total += batch_s
                 
-acc = correct/total
-print(f'Accuracy is: {acc:.3f}')
+acc = round(correct/total,4) * 100
+print(f'Accuracy of the MNIST Neural Network is: {acc}%')
 
 #Saving the parameters of the trained model
 model_path = 'mnist.pt'
 
-if acc > 0.90:
+if acc > 95.0:
     torch.save(model.state_dict(), model_path)
-    print('Model saved to ' + model_path)
+    print('Model parameters saved to ' + model_path)
